a48b61a4973aab194265de902bbab584772c885a
Akhi Singhania <akhi@dfinity.org>
2022-05-31 14:19:14 +0200

    Add similar rocksdb benchmarks as well

--- tests/rocks_db_benches.rs
+++ tests/rocks_db_benches.rs
@@ -1,15 +1,13 @@
 use std::time::Instant;
 
-use fast_kv_store::HashTable;
-use rand::seq::SliceRandom;
-use rand::Rng;
+use rand::{prelude::SliceRandom, Rng};
+use rocksdb::DB;
 use tempdir::TempDir;
 
 #[test]
-fn ht_benchmark_read() {
+fn rdb_benchmark_read() {
     let tmp_dir = TempDir::new("example").unwrap();
-    let salt = rand::thread_rng().gen::<[u8; 32]>();
-    let mut db = HashTable::new(tmp_dir.path().join("db"), salt, None);
+    let db = DB::open_default(tmp_dir.path().join("db")).unwrap();
 
     let num_elems = 10_000;
     let num_iter = 1_000_000;
@@ -25,8 +23,8 @@ fn ht_benchmark_read() {
         for _ in 0..rand::thread_rng().gen_range(0..1025) {
             value.push(rand::thread_rng().gen());
         }
-        db.set(key.clone(), value.clone());
-        assert_eq!(value, db.get(key.clone()).unwrap());
+        db.put(key.clone(), value.clone()).unwrap();
+        assert_eq!(value, db.get(key.clone()).unwrap().unwrap());
         keys.push(key);
         values.push(value);
     }
@@ -39,14 +37,13 @@ fn ht_benchmark_read() {
         db.get(key.clone()).unwrap();
     }
     let duration = start.elapsed();
-    println!("ht read ns {}", duration.as_nanos() / num_iter);
+    println!("rdb read ns {}", duration.as_nanos() / num_iter);
 }
 
 #[test]
 fn ht_benchmark_write() {
     let tmp_dir = TempDir::new("example").unwrap();
-    let salt = rand::thread_rng().gen::<[u8; 32]>();
-    let mut db = HashTable::new(tmp_dir.path().join("db"), salt, None);
+    let db = DB::open_default(tmp_dir.path().join("db")).unwrap();
 
     let num_elems = 10_000;
     let num_iter = 1_000_000;
@@ -74,18 +71,18 @@ fn ht_benchmark_write() {
         let index = indexes[i];
         let key = &keys[index];
         let value = &values[index];
-        db.set(key.clone(), value.clone());
+        db.put(key.clone(), value.clone()).unwrap();
     }
     let duration = start.elapsed();
     println!(
-        "ht first write ns {}",
+        "rdb first write ns {}",
         duration.as_nanos() / num_elems as u128
     );
 
     for i in 0..num_elems {
         let key = &keys[i];
         let value = &values[i];
-        assert_eq!(value, &db.get(key.clone()).unwrap());
+        assert_eq!(value, &db.get(key.clone()).unwrap().unwrap());
     }
 
     let indexes: Vec<usize> = (0..num_elems).collect();
@@ -94,8 +91,8 @@ fn ht_benchmark_write() {
         let index = indexes.choose(&mut rand::thread_rng()).unwrap().clone();
         let key = &keys[index];
         let value = &values[index];
-        db.set(key.clone(), value.clone());
+        db.put(key.clone(), value.clone()).unwrap();
     }
     let duration = start.elapsed();
-    println!("ht over write ns {}", duration.as_nanos() / num_iter);
+    println!("rdb over write ns {}", duration.as_nanos() / num_iter);
 }
